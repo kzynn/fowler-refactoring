64765997493849aeb855cda093208197597fe433
kzynn <herbstigel@gmx.net>
2020-05-12 23:21:18 +0200

    Add html statement

--- src/java/Customer.java
+++ src/java/Customer.java
@@ -32,6 +32,20 @@ class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        Enumeration enumRentals = rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        while (enumRentals.hasMoreElements()) {
+            Rental each = (Rental) enumRentals.nextElement();
+            //show figures for each rental
+            result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        //add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P> ";
+        return result;
+    }
+
     private double getTotalCharge() {
         double result = 0;
         Enumeration<Rental> enumRentals = rentals.elements();
